329d63771fb35f82a3e45ab49ead5ee3d0565411
iamhefang <he@hefang.link>
2023-03-19 10:37:20 +0800

    使用blob加载插件

--- src/plugin/types.ts
+++ src/plugin/types.ts
@@ -10,88 +10,83 @@ import { ThemeDefine, ThemeType } from "~/types"
 import { PlatformType } from "$hooks/usePlatform"
 
 export interface IPluginInfo {
-    id: string
-    name: string
-    version: string
-    logo?: string
-    description?: string
-    author?: string
-    license?: string
-    homepage?: string
-    repository?: string
-    dependencies?: string[]
-    /**
-     * 插件支持的平台和版本
-     */
-    supports: PluginSupport
-    /**
-     * 插件提供的能力
-     */
-    abilities?: PluginAbility[]
-    hooks?: PluginHookKeys[]
-    /**
-     * 插件提供的组件
-     */
-    components?: PluginComponents[]
-    enable?: boolean
+  id: string
+  name: string
+  version: string
+  logo?: string
+  description?: string
+  author?: string
+  license?: string
+  homepage?: string
+  repository?: string
+  dependencies?: string[]
+  /**
+   * 插件支持的平台和版本
+   */
+  supports: PluginSupport
+  /**
+   * 插件提供的能力
+   */
+  abilities?: PluginAbility[]
+  hooks?: PluginHookKeys[]
+  /**
+   * 插件提供的组件
+   */
+  components?: PluginComponents[]
+  enable?: boolean
 }
-export type PluginSupport = { platform: PlatformType[], version: string }
+export type PluginSupport = { platform: PlatformType[]; version: string }
 
 export type PluginAbility = keyof IPluginAbility
 export type PluginComponents = keyof IPluginComponents
-export type PluginHookKeys = keyof IPluginHooks;
-export interface IPlugin extends IPluginInfo, Partial<IPluginHooks>, Partial<IPluginComponents>, Partial<IPluginAbility>, Partial<IPluginLifecycle> {
-
-}
+export type PluginHookKeys = keyof IPluginHooks
+export interface IPlugin extends IPluginInfo, Partial<IPluginHooks>, Partial<IPluginComponents>, Partial<IPluginAbility>, Partial<IPluginLifecycle> {}
 
 export interface IEditorProps {
-    value: string
-    onChange: (value: string | undefined) => void
-    placeholder?: string
-    onFocus?: () => void
-    onBlur?: () => void
-    noteId: string
+  value: string
+  onChange: (value: string | undefined) => void
+  placeholder?: string
+  onFocus?: () => void
+  onBlur?: () => void
+  noteId: string
 }
 
 export interface IPluginHooks {
-    onThemeChange(theme: ThemeType, token: GlobalToken): void
+  onThemeChange(theme: ThemeType, token: GlobalToken): void
 }
 
 export interface IPluginLifecycle {
-    onInstall(): void
-    onUninstall(): void
-    onEnable(): void
-    onDisable(): void
-    onUpdate(): void
+  onInstall(): void
+  onUninstall(): void
+  onEnable(): void
+  onDisable(): void
+  onUpdate(): void
 }
 
-export type PluginTheme = ThemeConfig & { tooltip?: string, icon: ReactNode }
+export type PluginTheme = ThemeConfig & { tooltip?: string; icon: ReactNode }
 
-export type EditorComponent = (ComponentClass<IEditorProps> | FC<IEditorProps>) & { editorName?: string, options?: IEditorOptions[] }
+export type EditorComponent = (ComponentClass<IEditorProps> | FC<IEditorProps>) & { editorName?: string; options?: IEditorOptions[] }
 
-export interface IEditorOptions extends Omit<FormItemProps, "noStyle"> {
-
-}
+export interface IEditorOptions extends Omit<FormItemProps, "noStyle"> {}
 
 export interface IFooterTopComponentProps {
-    dispatch: typeof store.dispatch
-    settingSlice: typeof settingSlice
-    noteSlice: typeof noteSlice
-    uiSlice: typeof uiSlice
+  dispatch: typeof store.dispatch
+  settingSlice: typeof settingSlice
+  noteSlice: typeof noteSlice
+  uiSlice: typeof uiSlice
 }
 
 export type FooterTopComponent = (ComponentClass<IFooterTopComponentProps> | FC<IFooterTopComponentProps>) & {
-    order?: number
+  order?: number
 }
 
 export interface IPluginComponents {
-    Editor: EditorComponent
-    FooterLeft: FooterTopComponent
-    FooterRight: FooterTopComponent
-    TopLeft: FooterTopComponent
-    TopRight: FooterTopComponent
+  Editor: EditorComponent
+  FooterLeft: FooterTopComponent
+  FooterRight: FooterTopComponent
+  TopLeft: FooterTopComponent
+  TopRight: FooterTopComponent
 }
-
 export interface IPluginAbility {
-    theme: ThemeDefine
+  theme: ThemeDefine
 }
