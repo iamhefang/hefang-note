25e49295b9033c828afc09173c21056a2e4d940d
iamhefang <he@hefang.link>
2023-03-20 17:32:12 +0800

    完善新版本升级功能

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,6 @@
 
+import { Arch, OsType } from "@tauri-apps/api/os"
+
 import pkg from "^/package.json"
 
 export const versionName = pkg.version
@@ -6,3 +8,8 @@ export const productName = pkg.productName
 export const versionCode = parseInt(pkg.version.replace(/\./g, ""), 10)
 export const isInTauri = typeof window !== "undefined" && Reflect.has(window, "__TAURI_IPC__")
 export const isInWorker = typeof WorkerGlobalScope !== "undefined" && self instanceof WorkerGlobalScope
+
+export const clientUrls: Partial<Record<`${OsType}-${Arch}`, string>> = {
+    "Darwin-x86_64": "https://iamhefang.cn/tools/notebook/releases/何方笔记_x64.dmg",
+    "Windows_NT-x86_64": "https://iamhefang.cn/tools/notebook/releases/何方笔记_x64_zh-CN.msi",
+}
--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,12 +1,40 @@
-import { OsType, type } from "@tauri-apps/api/os"
-import { noop } from "lodash"
+import { arch, Arch, OsType, platform, Platform, type } from "@tauri-apps/api/os"
 import { useEffect, useState } from "react"
 
+import { isInTauri } from "~/consts"
+
 export type PlatformType = OsType | "Browser"
-export default function usePlatform(): PlatformType {
+export function usePlatformType(): PlatformType {
   const [value, setValue] = useState<PlatformType>("Browser")
   useEffect(() => {
-    type().then(setValue).catch(noop)
+    isInTauri && type().then(setValue).catch(console.error)
+  }, [])
+
+  return value
+}
+
+export function usePlatform(): Platform | undefined {
+  const [value, setValue] = useState<Platform>()
+  useEffect(() => {
+    isInTauri && platform().then(setValue).catch(console.error)
+  }, [])
+
+  return value
+}
+
+export function useArch(): Arch | undefined {
+  const [value, setValue] = useState<Arch>()
+  useEffect(() => {
+    isInTauri && arch().then(setValue).catch(console.error)
+  }, [])
+
+  return value
+}
+
+export function useOsTypr(): OsType | undefined {
+  const [value, setValue] = useState<OsType>()
+  useEffect(() => {
+    isInTauri && type().then(setValue).catch(console.error)
   }, [])
 
   return value
