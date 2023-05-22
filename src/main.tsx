3ddcd9ff3d6c94044520ab256ecae58bc9734d1b
iamhefang <he@hefang.link>
2023-05-22 23:18:37 +0800

    2023-05-22 23:18:37自动提交

--- src/main.tsx
+++ src/main.tsx
@@ -1,55 +1,50 @@
+import { Empty, message, Modal } from "antd"
+import dayjs from "dayjs"
+import "dayjs/locale/zh-cn"
+import relativeTime from "dayjs/plugin/relativeTime"
+import ReactDOMClient from "react-dom/client"
+import { Provider } from "react-redux"
+
+import Application from "./Application"
+import { versionName } from "./consts"
+import store from "./redux"
+
 import Html from "$components/utils/Html"
 import "$utils/debug"
 import "$utils/globals"
+import { html } from "^/CHANGELOG.md"
+import pkg from "^/package.json"
 
 import "$utils/worker"
 
-
-
-import {Empty, message, Modal} from "antd"
-import dayjs from "dayjs"
-import relativeTime from "dayjs/plugin/relativeTime"
-import ReactDOMClient from "react-dom/client"
-import {Provider} from "react-redux"
-
-import Application from "./Application"
-import {versionName} from "./consts"
-import store from "./redux"
-
-import {html} from "^/CHANGELOG.md"
-import pkg from "^/package.json"
 import "./style.scss"
 
+dayjs.locale("zh-cn")
 dayjs.extend(relativeTime)
 
-message.config({top: 40})
+message.config({ top: 40 })
 const root = ReactDOMClient.createRoot(document.getElementById("root") as HTMLElement)
-void navigator.locks.request("hefang-note", {ifAvailable: true}, async (lock) => {
-    if (lock) {
-        root.render(
-            <Provider store={store}>
-                <Application/>
-            </Provider>,
-        )
-        if (localStorage.getItem("firstRun") !== versionName) {
-            Modal.info({
-                title: "更新日志/Changelog",
-                content: <Html className="changelog-container" data-selectable>{html}</Html>,
-                okText: "知道了/OK",
-                width: "90%",
-                centered: true,
-                style: {maxWidth: 600},
-                onOk() {
-                    localStorage.setItem("firstRun", versionName)
-                },
-            })
-        }
-    } else {
-        root.render(<Empty
-            description={<span style={{color: "yellow"}}>{pkg.productName}已在其他窗口打开 / Application is running in other window</span>}
-        />)
+void navigator.locks.request("hefang-note", { ifAvailable: true }, async (lock) => {
+  if (lock) {
+    root.render(
+      <Provider store={store}>
+        <Application />
+      </Provider>,
+    )
+    if (localStorage.getItem("firstRun") !== versionName) {
+      Modal.info({
+        title: "更新日志/Changelog",
+        content: <Html className="changelog-container">{html}</Html>,
+        okText: "知道了/OK",
+        width: "90vw",
+        onOk() {
+          localStorage.setItem("firstRun", versionName)
+        },
+      })
     }
+  } else {
+    root.render(<Empty description={<span style={{ color: "yellow" }}>{pkg.productName}已在其他窗口打开 / Application is running in other window</span>} />)
+  }
 
-    return new Promise(() => {
-    })
+  return new Promise(() => {})
 })
---
+++ src/plugin/types.ts
@@ -0,0 +1,97 @@
+import { FormItemProps, GlobalToken, ThemeConfig } from "antd"
+import { ComponentClass, FC, ReactNode } from "react"
+
+import store from "~/redux"
+import { noteSlice } from "~/redux/noteSlice"
+import { settingSlice } from "~/redux/settingSlice"
+import { uiSlice } from "~/redux/uiSlice"
+import { NoteItem, ThemeDefine, ThemeType } from "~/types"
+
+import { PlatformType } from "$hooks/usePlatform"
+
+export interface IPluginInfo {
+    id: string
+    name: string
+    version: string
+    logo?: string
+    description?: string
+    author?: string
+    license?: string
+    homepage?: string
+    repository?: string
+    dependencies?: string[]
+    /**
+     * 插件支持的平台和版本
+     */
+    supports: PluginSupport
+    /**
+     * 插件提供的能力
+     */
+    abilities?: PluginAbility[]
+    hooks?: PluginHookKeys[]
+    /**
+     * 插件提供的组件
+     */
+    components?: PluginComponents[]
+    enable?: boolean
+}
+export type PluginSupport = { platform: PlatformType[], version: string }
+
+export type PluginAbility = keyof IPluginAbility
+export type PluginComponents = keyof IPluginComponents
+export type PluginHookKeys = keyof IPluginHooks;
+export interface IPlugin extends IPluginInfo, Partial<IPluginHooks>, Partial<IPluginComponents>, Partial<IPluginAbility>, Partial<IPluginLifecycle> {
+
+}
+
+export interface IEditorProps {
+    value: string
+    onChange: (value: string | undefined) => void
+    placeholder?: string
+    onFocus?: () => void
+    onBlur?: () => void
+    note: NoteItem
+}
+
+export interface IPluginHooks {
+    onThemeChange(theme: ThemeType, token: GlobalToken): void
+}
+
+export interface IPluginLifecycle {
+    onInstall(): void
+    onUninstall(): void
+    onEnable(): void
+    onDisable(): void
+    onUpdate(): void
+}
+
+export type PluginTheme = ThemeConfig & { tooltip?: string, icon: ReactNode }
+
+export type EditorComponent = (ComponentClass<IEditorProps> | FC<IEditorProps>) & { editorName?: string, options?: IEditorOptions[] }
+
+export interface IEditorOptions extends Omit<FormItemProps, "noStyle"> {
+
+}
+
+export interface IFooterTopComponentProps {
+    dispatch: typeof store.dispatch
+    settingSlice: typeof settingSlice
+    noteSlice: typeof noteSlice
+    uiSlice: typeof uiSlice
+}
+
+export type FooterTopComponent = (ComponentClass<IFooterTopComponentProps> | FC<IFooterTopComponentProps>) & {
+    order?: number
+}
+
+export interface IPluginComponents {
+    Editor: EditorComponent
+    FooterLeft: FooterTopComponent
+    FooterRight: FooterTopComponent
+    TopLeft: FooterTopComponent
+    TopRight: FooterTopComponent
+}
+
+export interface IPluginAbility {
+    theme: ThemeDefine
+}
