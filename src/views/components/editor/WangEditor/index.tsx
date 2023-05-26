f4635808c8045c5295e3980b13bf51ace128afee
iamhefang <he@hefang.link>
2023-05-26 23:59:04 +0800

    添加wangeditor

--- src/views/components/editor/WangEditor/index.tsx
+++ src/views/components/editor/WangEditor/index.tsx
@@ -1,94 +1,74 @@
-import {DomEditor, IDomEditor, IEditorConfig, IToolbarConfig} from "@wangeditor/editor"
-import {Editor, Toolbar} from "@wangeditor/editor-for-react"
+import { DomEditor, IDomEditor, IEditorConfig, IToolbarConfig } from "@wangeditor/editor"
+import { Editor, Toolbar } from "@wangeditor/editor-for-react"
+import { useEffect, useMemo, useState } from "react"
+
+import { EditorComponent } from "~/plugin/types"
 
 import "@wangeditor/editor/dist/css/style.css"
-import {App} from "antd"
-import {useCallback, useEffect, useMemo, useState} from "react"
-
-import {EditorComponent} from "~/plugin"
-
 import ss from "./index.module.scss"
 import useWangEditorTheme from "./useWangEditorTheme"
 
-const WangEditor: EditorComponent = ({value, onChange, onFocus, onBlur, placeholder, loading, noteId}) => {
-    const [editor, setEditor] = useState<IDomEditor | null>(null)
-    const {message} = App.useApp()
-    const toolbarConfig = useMemo<Partial<IToolbarConfig>>(
-        () => ({
-            toolbarKeys: [
-                "headerSelect",
-                "blockquote",
-                "bold",
-                "italic",
-                "underline",
-                "|",
-                "bulletedList",
-                "numberedList",
-                "insertLink",
-                "codeBlock",
-                "insertTable",
-                "emotion",
-            ],
-        }),
-        [],
-    )
-    const editorConfig = useMemo<Partial<IEditorConfig>>(
-        () => ({
-            placeholder,
-            onFocus: () => onFocus?.(),
-            onBlur: () => onBlur?.(),
-            customAlert: (info, type) => {
-                void message.open({type, content: info})
-            },
-            MENU_CONF: {
-                "uploadImage": {
-                    base64LimitSize: 5 * 1024 * 1024, // 5mb
-                },
-            },
-        }),
-        [message, onBlur, onFocus, placeholder],
-    )
+const WangEditor: EditorComponent = ({ value, onChange, onFocus, onBlur, placeholder }) => {
+  const [editor, setEditor] = useState<IDomEditor | null>(null)
+  const [text, setText] = useState("")
+  // const [innerValue, setInnerValue] = useState(value)
+  const toolbarConfig = useMemo<Partial<IToolbarConfig>>(
+    () => ({
+      toolbarKeys: [
+        "headerSelect",
+        "blockquote",
+        "bold",
+        "italic",
+        "underline",
+        "|",
+        "bulletedList",
+        "numberedList",
+        "insertLink",
+        "codeBlock",
+        "insertTable",
+        "emotion",
+      ],
+    }),
+    [],
+  )
+  const editorConfig = useMemo<Partial<IEditorConfig>>(
+    () => ({
+      placeholder,
+      onChange: (domEditor) => {
+        setText(domEditor.getText())
+      },
+      onFocus: () => onFocus?.(),
+      onBlur: () => onBlur?.(),
+    }),
+    [onBlur, onFocus, placeholder],
+  )
 
-    const theme = useWangEditorTheme()
-    useEffect(() => {
-        return () => {
-            if (!editor) {
-                return
-            }
-            DomEditor.getToolbar(editor)?.destroy()
-            editor?.destroy()
-            setEditor(null)
-        }
-    }, [editor])
+  useEffect(() => {
+    editor && onChange?.(editor.getHtml())
+  }, [editor, onChange, text])
 
-    const onValueChange = useCallback((domEditor: IDomEditor) => {
-        if (loading) {
-            return
-        }
-        console.info("onValueChange", noteId, domEditor.getHtml())
-        onChange?.(domEditor.isEmpty() ? "" : domEditor.getHtml())
-    }, [loading, noteId, onChange])
+  const theme = useWangEditorTheme()
+  useEffect(() => {
+    return () => {
+      if (!editor) {
+        return
+      }
+      DomEditor.getToolbar(editor)?.destroy()
+      editor?.destroy()
+      setEditor(null)
+    }
+  }, [editor])
+  // useEffect(() => {
+  //   console.info("收到新内容", value)
+  //   setInnerValue(value)
+  // }, [value])
 
-    return (
-        <div style={{height: "100%", display: "flex", flexDirection: "column", ...theme}} className={ss.editor}>
-            <Toolbar
-                editor={editor}
-                defaultConfig={toolbarConfig}
-                mode="simple"
-                style={{borderBottom: "1px solid var(--w-e-toolbar-border-color)"}}
-            />
-            <div style={{flex: 1, overflow: "hidden", position: "relative"}}>
-                <Editor
-                    defaultConfig={editorConfig}
-                    value={value}
-                    onChange={onValueChange}
-                    onCreated={setEditor}
-                    mode="simple"
-                    style={{position: "absolute", left: 0, top: 0, right: 0, bottom: 0}}
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div style={{ height: "100%", display: "flex", flexDirection: "column", ...theme }} className={ss.editor}>
+      <Toolbar editor={editor} defaultConfig={toolbarConfig} mode="default" style={{ borderBottom: "1px solid var(--w-e-toolbar-border-color)" }} />
+      <Editor defaultConfig={editorConfig} value={value} onCreated={setEditor} mode="default" style={{ flex: 1 }} />
+    </div>
+  )
 }
 
 export default WangEditor
