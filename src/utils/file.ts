71f95fdd68fa503dd19a0c30fd05586ebb10425b
iamhefang <he@hefang.link>
2023-06-01 17:22:54 +0800

    添加导出html和markdwon和拖动移动功能

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,34 +1,28 @@
-import {save} from "@tauri-apps/api/dialog"
-import {writeFile} from "@tauri-apps/api/fs"
+import { save } from "@tauri-apps/api/dialog"
+import { writeFile } from "@tauri-apps/api/fs"
 
-import {isInClient} from "~/consts"
+import { isInTauri } from "~/consts"
+
 
 export type DownloadFileOptions = {
     mimeType?: string
     fileName?: string
 }
-
-export async function saveFile(content: string, options?: DownloadFileOptions): Promise<void> {
-    return new Promise((resolve, reject) => {
-        if (isInClient) {
-            save({title: "保存文件", defaultPath: options?.fileName}).then(filePath => {
-                if (!filePath) {
-                    return
-                }
-                writeFile(filePath, content).then(resolve).catch(console.error)
-            }).catch(console.error)
-        } else {
-            const a = document.createElement("a")
-            a.style.display = "none"
-            const blob = new Blob([content], {type: options?.mimeType || "text/plain"})
-            a.href = window.URL.createObjectURL(blob)
-            a.download = options?.fileName || "何方笔记.txt"
-            document.body.appendChild(a)
-            a.click()
-            setTimeout(() => {
-                a.remove()
-                resolve()
-            }, 0)
-        }
-    })
+export function saveFile(content: string, options?: DownloadFileOptions) {
+    if (isInTauri) {
+        save({ title: "保存文件", defaultPath: options?.fileName }).then(filePath => {
+            if (!filePath) { return }
+            writeFile(filePath, content).catch(console.error)
+        }).catch(console.error)
+    } else {
+        const a = document.createElement("a")
+        a.style.display = "none"
+        const blob = new Blob([content], { type: options?.mimeType || "text/plain" })
+        const url = window.URL.createObjectURL(blob)
+        a.href = url
+        a.download = options?.fileName || "何方笔记.txt"
+        document.body.appendChild(a)
+        a.click()
+        setTimeout(() => a.remove(), 0)
+    }
 }
