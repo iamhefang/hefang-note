e751c76f0fc7321c7a75ff148f76111113cb929f
iamhefang <he@hefang.link>
2023-06-01 22:23:26 +0800

    修复更新日志 清理代码

--- src/hooks/noteTreeItem/uitils.ts
+++ src/hooks/noteTreeItem/uitils.ts
@@ -1,17 +1,16 @@
-import TurnDown from "turndown"
-import * as  turndownPluginGfm from "turndown-plugin-gfm"
+import { NoteItem } from "~/types"
 
-import {ContentIOType, PluginHookOccasion} from "~/plugin"
-import exportTplHTML from "~/templates/export-html.html?raw"
-import {NoteItem} from "~/types"
+const renamingMap: Record<string, string> = {
+    "new-note": "输入笔记名",
+    "new-dir": "输入目录名",
+}
 
-import ContentIOEvent, {ContentIODirection} from "$plugin/events/ContentIOEvent"
-import {callPluginsHook} from "$plugin/utils"
-import {contentStore} from "$utils/database"
-import {saveFile} from "$utils/file"
-
-const turndown = new TurnDown({headingStyle: "atx"})
-turndown.use(turndownPluginGfm.gfm)
+export function renamingPlaceholder(renamingId: string, item: NoteItem) {
+    return renamingMap[renamingId] ?? item.title
+}
+export function renamingDefaultValue(renamingId: string, item: NoteItem) {
+    return renamingId.startsWith("new") ? "" : item.title
+}
 
 export function getNewNote(item: Partial<NoteItem>): NoteItem {
     return {
@@ -22,55 +21,4 @@ export function getNewNote(item: Partial<NoteItem>): NoteItem {
         isLeaf: true,
         ...item,
     }
-}
-
-function convert4export(html: string, type: ContentIOType): [string, string, string] {
-    switch (type) {
-        case ContentIOType.markdown:
-            return [turndown.turndown(html), "text/markdown", "md"]
-        case ContentIOType.html:
-            return [html, "text/html", "html"]
-        default:
-            throw new Error("不支持的导出类型")
-    }
-}
-
-export async function doExport(item: NoteItem, type: ContentIOType): Promise<void> {
-    return new Promise((resolve, reject) => {
-        contentStore
-            .get(item.id)
-            .then((content) => {
-                const event = callPluginsHook("onContentExport", new ContentIOEvent({
-                    detail: {
-                        item,
-                        content,
-                        direction: ContentIODirection.export,
-                        type,
-                    },
-                    occasion: PluginHookOccasion.before,
-                }))
-                if (event.isDefaultPrevented()) {
-                    console.warn("插件阻止了导出")
-
-                    return reject("插件阻止了导出")
-                }
-                const title = event.detail.item.title
-                const exportHtml = exportTplHTML.replace("$TITLE_PLACEHOLDER$", title).replace("$CONTENT_PLACEHOLDER$", event.detail.content)
-                const [finalContent, mimeType, fileExt] = convert4export(exportHtml, type)
-                saveFile(finalContent, {mimeType, fileName: `${title}.${fileExt}`}).then(res => {
-                    callPluginsHook("onContentExport", new ContentIOEvent({
-                        detail: event.detail,
-                        occasion: PluginHookOccasion.after,
-                    }))
-                    resolve()
-                }).catch(error => {
-                    console.error("保存文件失败", error)
-                    reject(`保存文件失败: ${error}`)
-                })
-            })
-            .catch(error => {
-                console.error("导出失败", error)
-                reject(`导出失败: ${error}`)
-            })
-    })
 }
--- src/hooks/noteTreeItem/useTreeItemIcon.tsx
+++ src/hooks/noteTreeItem/useTreeItemIcon.tsx
@@ -1,23 +1,23 @@
-import {FileTextOutlined, FolderOpenOutlined, FolderOutlined} from "@ant-design/icons"
-import {useMemo} from "react"
+import { FileTextOutlined, FolderOpenOutlined, FolderOutlined } from "@ant-design/icons"
+import { useMemo } from "react"
 
-import {NoteItem} from "~/types"
+import { NoteItem } from "~/types"
 
 import useNoteLocked from "$hooks/useNoteLocked"
-import {useSettings} from "$hooks/useSelectors"
+import { useSettings } from "$hooks/useSelectors"
 
 export default function useTreeItemIcon(item: NoteItem) {
-    const {expandItems} = useSettings()
-    const noteLocked = useNoteLocked(item.id)
+  const { expandItems } = useSettings()
+  const noteLocked = useNoteLocked(item.id)
 
-    return useMemo(() => {
-        if (item.isLeaf) {
-            return <FileTextOutlined/>
-        }
-        if (noteLocked || !expandItems[item.id]) {
-            return <FolderOutlined/>
-        }
+  return useMemo(() => {
+    if (item.isLeaf) {
+      return <FileTextOutlined />
+    }
+    if (noteLocked || !expandItems[item.id]) {
+      return <FolderOutlined />
+    }
 
-        return <FolderOpenOutlined/>
-    }, [expandItems, item.id, item.isLeaf, noteLocked])
+    return <FolderOpenOutlined />
+  }, [expandItems, item.id, item.isLeaf, noteLocked])
 }
