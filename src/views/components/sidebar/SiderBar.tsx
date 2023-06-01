0b169b9f177f5267117e85137e221c54a103a514
iamhefang <he@hefang.link>
2023-06-01 21:54:16 +0800

    添加搜索结果关键字高亮

--- src/views/components/sidebar/SiderBar.tsx
+++ src/views/components/sidebar/SiderBar.tsx
@@ -1,12 +1,13 @@
+import useSearchValue from "$hooks/useSearchValue"
 import {MoreOutlined} from "@ant-design/icons"
 import {Button, Col, Dropdown, Input, Row, Skeleton} from "antd"
 import {debounce} from "lodash"
-import React, {ChangeEvent, useCallback} from "react"
+import React, {ChangeEvent, useCallback, useMemo, useState} from "react"
+
+import useSiderBarTopMenuItems from "./hooks/useSiderBarTopMenuItems"
 
 import NoteTree from "$components/tree/NoteTree"
-import useSearchValue from "$hooks/useSearchValue"
 import {useNotes} from "$hooks/useSelectors"
-import useSiderBarTopMenuItems from "$hooks/useSiderBarTopMenuItems"
 import {useTranslate} from "$hooks/useTranslate"
 
 export default function SiderBar() {
---
+++ src/views/components/tree/hooks/useTreeItemTitle.tsx
@@ -0,0 +1,44 @@
+import {Input} from "antd"
+import {useMemo} from "react"
+
+import {NAME_MAX_LENGTH} from "~/config"
+import {NoteItem} from "~/types"
+
+import useOnRenameKeyDown from "./useOnRenameKeyDown"
+import useOnRenamingBlur from "./useOnRenamingBlur"
+
+import {stopPropagation} from "$components/utils/event"
+import Html from "$components/utils/Html"
+import useSearchValue from "$hooks/useSearchValue"
+import {useNotes} from "$hooks/useSelectors"
+import {useThemeConfig} from "$hooks/useThemeConfig"
+
+export default function useTreeItemTitle(item: NoteItem) {
+    const onRenamingBlur = useOnRenamingBlur(item)
+    const onRenameKeyDown = useOnRenameKeyDown(item)
+    const {renamingId} = useNotes()
+    const [search] = useSearchValue()
+    const {token} = useThemeConfig()
+
+    return useMemo(
+        () =>
+            renamingId === item.id ? (
+                <Input
+                    key={`rename-input-${item.id}`}
+                    autoFocus
+                    size="small"
+                    placeholder={item.title}
+                    defaultValue={item.title}
+                    onKeyDown={onRenameKeyDown}
+                    onBlur={onRenamingBlur}
+                    maxLength={NAME_MAX_LENGTH}
+                    onContextMenu={stopPropagation}
+                />
+            ) : (
+                <Html title={item.title} style={{overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap"}}>
+                    {search ? item.title.replaceAll(search, `<span style="color:${token?.colorPrimary}">${search}</span>`) : item.title}
+                </Html>
+            ),
+        [item.id, item.title, onRenameKeyDown, onRenamingBlur, renamingId, search, token?.colorPrimary],
+    )
+}
