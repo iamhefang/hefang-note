8305bee6fbdf7df91762c7d57dac0e2d15f3b438
iamhefang <he@hefang.link>
2023-06-01 12:29:22 +0800

    编辑器优化

--- src/views/components/editor/EditorArea.tsx
+++ src/views/components/editor/EditorArea.tsx
@@ -1,6 +1,5 @@
 import { theme as antTheme, Empty } from "antd"
-import { throttle } from "lodash"
-import { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react"
 
 import { CONTENT_SAVE_DELAY } from "~/config"
 import useNoteLocked from "~/hooks/useNoteLocked"
@@ -19,13 +18,13 @@ import { contentStore } from "$utils/database"
 
 export default function EditorArea() {
   const t = useTranslate()
-  const { ids } = useNotes()
   const { editorOptions, editor } = useSettings()
   const {
     token: { colorBgLayout, colorText },
   } = antTheme.useToken()
   const dispatch = useAppDispatch()
   const [changing, setChanging] = useState(false)
+  const [reading, setReading] = useState(false)
   const [value, setValue] = useState("")
   const current = useCurrent()
   const plugins = usePluginMap()
@@ -33,36 +32,42 @@ export default function EditorArea() {
     return editor && plugins[editor]?.Editor ? plugins[editor].Editor! : WangEditor
   }, [editor, plugins])
 
-  const refSaveTimer = useRef(0)
+  const refSaveTimer = useRef<number>(0)
   const onValueChange = useCallback((newValue: string | undefined) => {
     setChanging(true)
     setValue(newValue || "")
   }, [])
 
   useEffect(() => {
+    refSaveTimer.current && window.clearTimeout(refSaveTimer.current)
+    if (!current?.isLeaf || !current?.id || reading) {
+      return
+    }
     window.setTimeout(() => {
-      if (!current?.isLeaf || !current?.id) {
-        return
-      }
       const newContent = { id: current.id, content: value }
       console.info("正在保存笔记", newContent)
-      setChanging(false)
       dispatch(updateContent(newContent))
+      setChanging(false)
     }, CONTENT_SAVE_DELAY)
 
     return () => {
-      refSaveTimer.current && window.clearTimeout(refSaveTimer.current)
+      // refSaveTimer.current && window.clearTimeout(refSaveTimer.current)
     }
-  }, [current?.id, current?.isLeaf, dispatch, value])
+  }, [current?.id, current?.isLeaf, dispatch, reading, value])
 
   const noteLocked = useNoteLocked(current?.id)
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (!current?.isLeaf || !current?.id) {
       setValue("")
 
       return
     }
-    contentStore.get(current?.id, "").then(setValue).catch(console.error)
+    setReading(true)
+    contentStore
+      .get(current?.id, "")
+      .then(setValue)
+      .catch(console.error)
+      .finally(() => setReading(false))
   }, [current?.id, current?.isLeaf])
 
   if (noteLocked) {
@@ -83,7 +88,7 @@ export default function EditorArea() {
       }}
     >
       <div className="editor-wrapper" style={editorOptions}>
-        <Editor note={current} value={value} onChange={onValueChange} placeholder={t("尽情记录吧")} />
+        <Editor value={value} onChange={onValueChange} placeholder={t("尽情记录吧")} />
       </div>
     </div>
   )
