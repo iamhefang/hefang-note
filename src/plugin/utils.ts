674300fa61758ba952a89337a64bf5633c80abc7
iamhefang <he@hefang.link>
2023-06-03 22:05:48 +0800

    更改插件hook调用方式

---
+++ src/plugin/utils.ts
@@ -0,0 +1,14 @@
+import {PluginHookKeys} from "$plugin/defines"
+import {PluginHookEvent} from "$plugin/events"
+
+export function callPluginsHook<D, E extends PluginHookEvent<D>>(hook: PluginHookKeys, event: E): E {
+    for (const plugin of window.notebook.plugins) {
+        if (!event.bubble) {
+            break
+        }
+        // @ts-ignore
+        plugin.hooks?.includes(hook) && plugin[hook]?.(event)
+    }
+
+    return event
+}
