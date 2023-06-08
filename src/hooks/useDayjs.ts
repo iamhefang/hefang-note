e0bafd0587701b596d8518917f5d77cc2d39c8a4
iamhefang <he@hefang.link>
2023-06-08 16:57:29 +0800

    修复语言变化时dayjs语言不会立即变化问题

---
+++ src/hooks/useDayjs.ts
@@ -0,0 +1,14 @@
+import dayjs from "dayjs"
+import {useMemo} from "react"
+
+import {useLocaleDefine} from "$hooks/useTranslate"
+
+export default function useDayjs(): typeof dayjs {
+    const locale = useLocaleDefine()
+
+    return useMemo(() => {
+        dayjs.locale(locale.dayjs)
+
+        return dayjs
+    }, [locale.dayjs])
+}
