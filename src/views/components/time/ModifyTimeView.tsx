f847c3a5ccd05a1304c17cec045992538f050c04
iamhefang <he@hefang.link>
2023-06-08 15:40:10 +0800

    添加一些漏掉的翻译

--- src/views/components/time/ModifyTimeView.tsx
+++ src/views/components/time/ModifyTimeView.tsx
@@ -3,20 +3,22 @@ import {useMemo} from "react"
 
 import useCurrent from "$hooks/useCurrent"
 import {useSettings} from "$hooks/useSelectors"
+import {useTranslate} from "$hooks/useTranslate"
 
 export default function ModifyTimeView() {
     const current = useCurrent()
     const {
         showEditTime,
     } = useSettings()
+    const t = useTranslate()
 
     return useMemo(() => (
             current?.modifyTime && showEditTime ? <span>
-                最近编辑: <span
+                {t("最近编辑")}: <span
                 title={dayjs(current?.modifyTime).format("YYYY-M-D H:m:s")}
                 >{dayjs(current?.modifyTime).fromNow()}</span>
             </span> : null
         ),
-        [current?.modifyTime, showEditTime],
+        [current?.modifyTime, showEditTime, t],
     )
 }
