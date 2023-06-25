cd617f88b178a8f04e4b99420da0d661ac0ae52f
iamhefang <he@hefang.link>
2023-06-25 17:24:51 +0800

    完善插件商店

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,52 +1,51 @@
-import {App as Antd, ConfigProvider} from "antd"
-import dayjs from "dayjs"
-import React, {Suspense, useEffect} from "react"
+import { App as Antd, ConfigProvider } from "antd"
+import React, { Suspense, useEffect } from "react"
 
-import {uiSlice} from "~/redux/uiSlice"
+import { uiSlice } from "~/redux/uiSlice"
 import View from "~/views"
 
-import {useAppDispatch} from "./redux"
+import { useAppDispatch } from "./redux"
 
 import Loading from "$components/loading/Loading"
 import useContentLoader from "$hooks/useContentLoader"
-import {useStates} from "$hooks/useSelectors"
+import { useStates } from "$hooks/useSelectors"
 import useSettingsLoader from "$hooks/useSettingsLoader"
-import {useThemeConfig} from "$hooks/useThemeConfig"
-import {useLocaleDefine} from "$hooks/useTranslate"
+import { useThemeConfig } from "$hooks/useThemeConfig"
+import { useLocaleDefine } from "$hooks/useTranslate"
 import usePluginComponents from "$plugin/hooks/usePluginComponents"
 import usePluginEffect from "$plugin/hooks/usePluginEffect"
 
 const LazySettings = React.lazy(async () => import("~/views/settings"))
 
 export default function Application() {
-    const {launching} = useStates()
-    const loadContents = useContentLoader()
-    const loadSettings = useSettingsLoader()
-    const dispatch = useAppDispatch()
-    useEffect(() => {
-        void (async () => {
-            await loadSettings()
-            dispatch(uiSlice.actions.ready({}))
-            void loadContents()
-        })()
-    }, [dispatch, loadContents, loadSettings])
+  const { launching } = useStates()
+  const loadContents = useContentLoader()
+  const loadSettings = useSettingsLoader()
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    void (async () => {
+      await loadSettings()
+      dispatch(uiSlice.actions.ready({}))
+      void loadContents()
+    })()
+  }, [dispatch, loadContents, loadSettings])
 
-    usePluginEffect()
-    const themeConfig = useThemeConfig()
-    const locale = useLocaleDefine()
-    const components = usePluginComponents("Float")
+  usePluginEffect()
+  const themeConfig = useThemeConfig()
+  const locale = useLocaleDefine()
+  const components = usePluginComponents("Float")
 
-    return launching ? (
-        <Loading/>
-    ) : (
-        <ConfigProvider autoInsertSpaceInButton={false} locale={locale.antd} theme={themeConfig}>
-            <Antd message={{top: 40}} notification={{top: 40}}>
-                <View/>
-                <Suspense>
-                    <LazySettings/>
-                </Suspense>
-                {components}
-            </Antd>
-        </ConfigProvider>
-    )
+  return launching ? (
+    <Loading />
+  ) : (
+    <ConfigProvider autoInsertSpaceInButton={false} locale={locale.antd} theme={themeConfig}>
+      <Antd message={{ top: 40 }} notification={{ top: 40 }}>
+        <View />
+        <Suspense>
+          <LazySettings />
+        </Suspense>
+        {components}
+      </Antd>
+    </ConfigProvider>
+  )
 }
