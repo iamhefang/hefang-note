33da1b05c6979c9362184711baed4257a4751474
iamhefang <he@hefang.link>
2023-07-02 11:06:31 +0800

    调整鼠标图标

--- src/views/components/settings/useShortcutSettings.tsx
+++ src/views/components/settings/useShortcutSettings.tsx
@@ -1,25 +1,31 @@
-import {Form} from "antd"
-import {ReactNode, useMemo} from "react"
+import { Form } from "antd"
+import { ReactNode, useMemo } from "react"
+
+import { isInClient } from "~/consts"
 
 import ShortcutsView from "$components/shortcusts/ShortcutsView"
-import {useTranslate} from "$hooks/useTranslate"
+import { useTranslate } from "$hooks/useTranslate"
 
 export default function useShortcutSettings(): Record<string, ReactNode> {
-    const t = useTranslate()
+  const t = useTranslate()
 
-    return useMemo(
-        () => ({
-            [t("锁定软件")]: (
-                <Form.Item name={["shortcut", "lock"]} noStyle>
-                    <ShortcutsView/>
-                </Form.Item>
-            ),
-            [t("关闭窗口")]: (
-                <Form.Item name={["shortcut", "closeWindow"]} noStyle>
-                    <ShortcutsView/>
-                </Form.Item>
-            ),
-        }),
-        [t],
-    )
+  return useMemo(() => {
+    const shortcuts = {
+      [t("锁定软件")]: (
+        <Form.Item name={["shortcut", "lock"]} noStyle>
+          <ShortcutsView />
+        </Form.Item>
+      ),
+    }
+
+    if (isInClient) {
+      shortcuts[t("关闭窗口")] = (
+        <Form.Item name={["shortcut", "closeWindow"]} noStyle>
+          <ShortcutsView />
+        </Form.Item>
+      )
+    }
+
+    return shortcuts
+  }, [t])
 }
