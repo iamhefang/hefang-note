3ad6fc58024a0a1a2139ae277cfbdbadd9d2bd12
iamhefang <he@hefang.link>
2023-07-02 11:06:31 +0800

    调整鼠标图标

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,3 +1,7 @@
-export function hash(hashType: string, input: string) { 
-    
+export async function digestHash(hashType: string, input: string) {
+    const digest = await crypto.subtle.digest(hashType, new TextEncoder().encode(input))
+
+    return Array.from(new Uint8Array(digest))
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join("")
 }
