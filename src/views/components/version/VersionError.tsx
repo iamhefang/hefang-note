9c79f0771543b213fcfa8de89bd1e81d65cc22f1
iamhefang <he@hefang.link>
2023-07-14 09:50:43 +0800

    2023-07-14 09:50:43自动提交

--- src/views/components/version/VersionError.tsx
+++ src/views/components/version/VersionError.tsx
@@ -1,13 +1,18 @@
 import { WarningOutlined } from "@ant-design/icons"
+import { theme } from "antd"
 import { useCallback, useMemo } from "react"
 
 import { isInClient } from "~/consts"
 
 import ss from "./VersionError.module.scss"
 
+import WindowControls from "$components/topbar/items/WindowControls"
+import ShowInPlatform from "$components/utils/ShowInPlatform"
 import useCheckUpdate from "$hooks/useCheckUpdate"
+
 export default function VersionError() {
-  const [doCheck, status] = useCheckUpdate()
+  const { token } = theme.useToken()
+  const [doCheck] = useCheckUpdate()
   const doUpgrade = useCallback(async () => {
     if (isInClient) {
       doCheck()
@@ -32,9 +37,16 @@ export default function VersionError() {
   }, [doUpgrade])
 
   return (
-    <div className={ss.root} data-tauri-drag-region>
+    <div className={ss.root} data-tauri-drag-region style={{ color: token.colorText, background: token.colorBgLayout }}>
+      <ShowInPlatform platforms={["Linux", "Windows_NT"]}>
+        {() => (
+          <div className={ss.controls}>
+            <WindowControls />
+          </div>
+        )}
+      </ShowInPlatform>
       <div>
-        <WarningOutlined style={{ fontSize: 100 }} />
+        <WarningOutlined style={{ fontSize: 100, color: token.colorPrimary }} />
       </div>
       <div>您当前使用的版本过低</div>
       {notice}
