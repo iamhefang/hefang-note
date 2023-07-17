08da7b778270cbab30b0f6df499e8ee46875ea4d
iamhefang <he@hefang.link>
2023-07-17 16:36:06 +0800

    重命名或新建笔记时自动全选默认值

--- src/views/components/locker/NoteUnlocker.tsx
+++ src/views/components/locker/NoteUnlocker.tsx
@@ -1,49 +1,54 @@
-import {LockOutlined} from "@ant-design/icons"
-import {Input, Result} from "antd"
-import React, {useCallback} from "react"
+import { LockOutlined } from "@ant-design/icons"
+import { Input, Result, theme } from "antd"
+import React, { useCallback } from "react"
 
-import {useSettings} from "~/hooks/useSelectors"
-import {useAppDispatch} from "~/redux"
-import {setItemsExpanded} from "~/redux/settingSlice"
-import {unlockContent} from "~/redux/uiSlice"
-import {NoteItem} from "~/types"
+import { useSettings } from "~/hooks/useSelectors"
+import { useAppDispatch } from "~/redux"
+import { setItemsExpanded } from "~/redux/settingSlice"
+import { unlockContent } from "~/redux/uiSlice"
+import { NoteItem } from "~/types"
 
-import {useTranslate} from "$hooks/useTranslate"
+import { useTranslate } from "$hooks/useTranslate"
 
 export type NoteUnlockerProps = {
-    item: NoteItem
+  item: NoteItem
 }
-export default function NoteUnlocker({item}: NoteUnlockerProps) {
-    const {lockedContents, unlockContentByAppLockPassword, lock: {password}} = useSettings()
-    const t = useTranslate()
-    const dispatch = useAppDispatch()
-    const onChange = useCallback(
-        (e: React.ChangeEvent<HTMLInputElement>) => {
-            const matchNotePwd = lockedContents[item.id] === e.currentTarget.value
-            const matchAppPwd = unlockContentByAppLockPassword && password === e.currentTarget.value
-            if (matchNotePwd || matchAppPwd) {
-                dispatch(unlockContent(item.id))
-                item.isLeaf || dispatch(setItemsExpanded({[item.id]: true}))
-            }
-        },
-        [dispatch, item.id, item.isLeaf, lockedContents, password, unlockContentByAppLockPassword],
-    )
+export default function NoteUnlocker({ item }: NoteUnlockerProps) {
+  const {
+    lockedContents,
+    unlockContentByAppLockPassword,
+    lock: { password },
+  } = useSettings()
+  const t = useTranslate()
+  const { token } = theme.useToken()
+  const dispatch = useAppDispatch()
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const matchNotePwd = lockedContents[item.id] === e.currentTarget.value
+      const matchAppPwd = unlockContentByAppLockPassword && password === e.currentTarget.value
+      if (matchNotePwd || matchAppPwd) {
+        dispatch(unlockContent(item.id))
+        item.isLeaf || dispatch(setItemsExpanded({ [item.id]: true }))
+      }
+    },
+    [dispatch, item.id, item.isLeaf, lockedContents, password, unlockContentByAppLockPassword],
+  )
 
-    return (
-        <Result
-            style={{height: "100%", display: "flex", flexDirection: "column", justifyContent: "center"}}
-            title={item.title}
-            subTitle={t(`该${item.isLeaf ? "笔记" : "目录"}已锁定，请输入解锁密码`)}
-            icon={<LockOutlined/>}
-            extra={
-                <Input.Password
-                    placeholder={t("请输入解锁密码")}
-                    allowClear
-                    maxLength={6}
-                    onChange={onChange}
-                    style={{maxWidth: 180}}
-                />
-            }
+  return (
+    <Result
+      style={{ height: "100%", display: "flex", flexDirection: "column", justifyContent: "center" }}
+      title={item.title}
+      subTitle={t(`该${item.isLeaf ? "笔记" : "目录"}已锁定，请输入解锁密码`)}
+      icon={<LockOutlined style={{ color: token.colorPrimary }} />}
+      extra={
+        <Input.Password
+          placeholder={t("请输入解锁密码")}
+          allowClear
+          maxLength={6}
+          onChange={onChange}
+          style={{ maxWidth: 180 }}
         />
-    )
+      }
+    />
+  )
 }
